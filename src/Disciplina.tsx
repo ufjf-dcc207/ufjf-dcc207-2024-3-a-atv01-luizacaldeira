1ee9bb9ce5e21ed7d08f7ef1c5193e6cf6867e78
Luiza Caldeira <luiza.caldeira@estudante.ufjf.br>
2024-11-11 20:35:54 -0300

    Adiciona estilos

--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -1,7 +1,10 @@
+import "./Disciplina.css";
+
 function Disciplina(){
-    return <div>
+    return <div className="disciplina">
         <div>DCC207</div>
         <div>Desenvolvimento Web Front-End</div>
     </div>;
 }
 export default Disciplina;
+
